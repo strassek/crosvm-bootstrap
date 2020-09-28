0c490241d58553f89d0426703557a19129b3260c
Kalyan Kondapally <kalyan.kondapally@intel.com>
2020-09-28 04:11:50 -0700

    possible demos fix
    Signed-off-by: Kalyan Kondapally <kalyan.kondapally@intel.com>

--- common/scripts/build_demos.sh
+++ common/scripts/build_demos.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-# build-demos.sh
-# Builds all demos.
+# package-builder.sh
+# Builds all needed drivers, cros_vm and other needed packages.
 
 # exit on any script line that fails
 set -o errexit
@@ -29,12 +29,12 @@ if [ $BUILD_CHANNEL == "--dev" ]; then
 LOCAL_CHANNEL=dev
 fi
 
-if [ $BUILD_TARGET == "--debug" ]; then
-LOCAL_BUILD_TARGET=debug
+if [ $BUILD_TYPE == "--debug" ]; then
+LOCAL_BUILD_TYPE=debug
 fi
 
-LOCAL_CURRENT_WLD_PATH=/opt/$LOCAL_CHANNEL/$LOCAL_BUILD_TARGET/x86
-LOCAL_MESON_BUILD_DIR=build.$LOCAL_BUILD_TARGET.x86
+LOCAL_CURRENT_WLD_PATH=/opt/$LOCAL_CHANNEL/$LOCAL_BUILD_TYPE/x86_64
+LOCAL_MESON_BUILD_DIR=build.$LOCAL_BUILD_TYPE.x86_64
 
 # Export environment variables
 export C_INCLUDE_PATH=$LOCAL_CURRENT_WLD_PATH/include:$LOCAL_CURRENT_WLD_PATH/include/libdrm/
@@ -43,30 +43,26 @@ export CPATH=$LOCAL_CURRENT_WLD_PATH/include:$LOCAL_CURRENT_WLD_PATH/include/lib
 export PATH="$PATH:$LOCAL_CURRENT_WLD_PATH/include/:$LOCAL_CURRENT_WLD_PATH/include/libdrm/:$LOCAL_CURRENT_WLD_PATH/bin"
 export ACLOCAL_PATH=$LOCAL_CURRENT_WLD_PATH/share/aclocal
 export ACLOCAL="aclocal -I $ACLOCAL_PATH"
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/lib/i386-linux-gnu/pkgconfig:/lib/i386-linux-gnu/pkgconfig
-export PKG_CONFIG_PATH_FOR_BUILD=$PKG_CONFIG_PATH
-export LD_LIBRARY_PATH=$LOCAL_CURRENT_WLD_PATH/lib
+export PKG_CONFIG_PATH=$LOCAL_CURRENT_WLD_PATH/lib/x86_64-linux-gnu/pkgconfig:$LOCAL_CURRENT_WLD_PATH/lib/pkgconfig:$LOCAL_CURRENT_WLD_PATH/share/pkgconfig:/lib/x86_64-linux-gnu/pkgconfig
+export LD_LIBRARY_PATH=$LOCAL_CURRENT_WLD_PATH/lib/x86_64-linux-gnu:$LOCAL_CURRENT_WLD_PATH/lib
 export PATH="$PATH:$LOCAL_CURRENT_WLD_PATH/bin"
-export CC=/usr/bin/i686-linux-gnu-gcc
-
-LOCAL_COMPILER_OPTIONS="--host=i686-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32" --prefix=$LOCAL_CURRENT_WLD_PATH"
 
 # Set Working Build directory based on the channel.
 WORKING_DIR=/build/$LOCAL_CHANNEL/demos
 
 # Print all environment settings
 echo "Build settings being used....."
-if [ $BUILD_TARGET == "--release" ]; then
+if [ $BUILD_TYPE == "--release" ]; then
   echo "Build Type: Release"
 else
   echo "Build Type: Debug"
 fi
 
 echo "Channel:" $LOCAL_CHANNEL
-if [ $BUILD_TYPE == "--clean" ]; then
-  echo "Clean" $LOCAL_BUILD_TARGET "Build"
+if [ $CLEAN_BUILD == "--clean" ]; then
+  echo "Clean" $LOCAL_BUILD_TYPE "Build"
 else
-  echo "Incremental" $LOCAL_BUILD_TARGET "Build"
+  echo "Incremental" $LOCAL_BUILD_TYPE "Build"
 fi
 
 echo "Working Directory:" $WORKING_DIR
@@ -84,7 +80,7 @@ cd /build
 FORCE_CONFIGURE=0
 CUSTOM_APP_OPTIONS=""
 function make_clean_asneeded() {
-if [ $BUILD_TYPE == "--clean" ]; then
+if [ $CLEAN_BUILD == "--clean" ]; then
   if [ "$(find . | grep -i '.*[.]o' | wc -c)" == 0 ]; then
     echo "make clean called"
     make clean || true
@@ -98,12 +94,12 @@ fi
 function configure_asneeded() {
 if [ $FORCE_CONFIGURE == 1 ]; then
   echo "Configuring the project"
-  ./autogen.sh --prefix=$LOCAL_CURRENT_WLD_PATH $LOCAL_COMPILER_OPTIONS
+  ./autogen.sh --prefix=$LOCAL_CURRENT_WLD_PATH
   FORCE_CONFIGURE=0
 else
   if [ "$(find . | grep -i '.*[.]o' | wc -c)" != 0 ]; then
     echo "Configuring the project"
-    ./autogen.sh --prefix=$LOCAL_CURRENT_WLD_PATH $LOCAL_COMPILER_OPTIONS
+    ./autogen.sh --prefix=$LOCAL_CURRENT_WLD_PATH
     FORCE_CONFIGURE=0
   else
     echo "Skipped configuring the project as this is incremental build."
@@ -115,7 +111,7 @@ function autogen_build() {
 #make_clean_asneeded
 #configure_asneeded
 make clean || true
-./autogen.sh --prefix=$LOCAL_CURRENT_WLD_PATH $CUSTOM_APP_OPTIONS $LOCAL_COMPILER_OPTIONS
+./autogen.sh --prefix=$LOCAL_CURRENT_WLD_PATH $CUSTOM_APP_OPTIONS
 make install
 CUSTOM_APP_OPTIONS=""
 }
@@ -133,7 +129,7 @@ echo "Building demos............"
 echo "Building glew............"
 cd $WORKING_DIR/glew
 make extensions
-make install GLEW_DEST=$LOCAL_CURRENT_WLD_PATH GLEW_PREFIX=$LOCAL_CURRENT_WLD_PATH $LOCAL_COMPILER_OPTIONS
+make install GLEW_DEST=$LOCAL_CURRENT_WLD_PATH GLEW_PREFIX=$LOCAL_CURRENT_WLD_PATH
 
 echo "Building glu............"
 cd $WORKING_DIR/glu
@@ -147,5 +143,6 @@ autogen_build
 echo "Building xdpyinfo............"
 cd $WORKING_DIR/xdpyinfo
 autogen_build
+
 
 
