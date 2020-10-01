f066aefa1dc11216bad847468c0c67793f9a7791
Kalyan Kondapally <kalyan.kondapally@intel.com>
2020-09-30 20:02:26 -0700

    Remove need for sudo when cloning code.
    Signed-off-by: Kalyan Kondapally <kalyan.kondapally@intel.com>

--- sync_code.sh
+++ sync_code.sh
@@ -10,13 +10,18 @@ set -o nounset
 # bail on failing commands before last pipe
 set -o pipefail
 
+CHANNEL=${1:-"--stable"}
+
 PWD_DIR=$PWD
 mkdir -p $PWD_DIR/source
 cd $PWD_DIR/source
+mkdir -p $PWD_DIR/source/env
 # Repo initialization and cloning all needed Libraries.
 if [ ! -f "/usr/bin/python" ]; then
-ln -s /usr/bin/python3 /usr/bin/python
+ln -s /usr/bin/python3 $PWD_DIR/source/env/python
 fi
+
+export PATH=$PATH:$PWD_DIR/source/env/
 
 if [ ! -d "depot_tools" ]; then
   echo "Cloning Depot Tools."
@@ -30,14 +35,18 @@ fi
 cd depot_tools/
 export PATH=$PWD:$PATH
 
-cd $PWD_DIR/source
-mkdir -p stable
-cd stable
-repo init -u  https://github.com/kalyankondapally/manifest.git -m dev.xml
-repo sync
+if [ $CHANNEL == "--stable" ] || [ $CHANNEL == "--all" ]; then
+  cd $PWD_DIR/source
+  mkdir -p stable
+  cd stable
+  repo init -u  https://github.com/kalyankondapally/manifest.git -m dev.xml
+  repo sync
+fi
 
-cd $PWD_DIR/source
-mkdir -p dev
-cd dev
-repo init -u  https://github.com/kalyankondapally/manifest.git -m dev.xml
-repo sync
+if [ $CHANNEL == "--dev" ] || [ $CHANNEL == "--all" ]; then
+  cd $PWD_DIR/source
+  mkdir -p dev
+  cd dev
+  repo init -u  https://github.com/kalyankondapally/manifest.git -m dev.xml
+  repo sync
+fi
