8bef48099234023ddd4e046b05a398dea453f909
kalyan <kalyan@localhost.localdomain>
2020-11-03 21:54:27 -0800

    Initial support to build on CentOS.
    Doesn't compile yet.
    
    Signed-off-by: kalyan <kalyan@localhost.localdomain>

--- tools/install_system_dependencies.sh
+++ tools/install_system_dependencies.sh
@@ -14,20 +14,19 @@ set -o pipefail
 EXIT_CODE=0
 
 ######Globals ####################################################
-
+LOCAL_OS=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
 ###############################################################################
 ##install_package()
 ###############################################################################
 function install_package() {
 	package_name="${1}"
-	if [[ ! "$(dpkg -s $package_name)" ]]; then
-  		echo "installing:" $package_name "----------------"
-  		sudo apt-get install -y  --no-install-recommends --no-install-suggests $package_name
-  		sudo apt-mark hold $package_name
-  		echo "---------------------"
+	echo "installing:" $package_name "----------------"
+	if [[ "$LOCAL_OS" == *"CentOS"* ]]; then
+		sudo yum install $package_name -y
 	else
-  		echo $package_name "is already installed."
+  		sudo apt-get install -y $package_name
 	fi
+  	echo "---------------------"
 }
 
 ###############################################################################
@@ -35,24 +34,30 @@ function install_package() {
 ###############################################################################
 install_package git 
 install_package curl 
-install_package libncurses-dev 
-install_package libssl-dev 
-install_package ccache 
-install_package bison 
-install_package flex 
-install_package libelf-dev 
-install_package build-essential 
-install_package python3 
-install_package dkms 
-install_package libudev-dev 
-install_package libpci-dev 
-install_package libiberty-dev autoconf
-install_package docker 
-install_package docker.io 
-install_package docker-compose 
-install_package pciutils 
-install_package debian-archive-keyring 
-install_package zlib1g-dev 
-install_package ca-certificates 
-install_package debootstrap 
-install_package gnome-session-wayland
+install_package ca-certificates  
+
+if [[ "$LOCAL_OS" == *"CentOS"* ]]; then
+	install_package yum-utils
+	#install_package debian-keyring-2017.5-2.el7.noarch
+	install_package gnome-session-wayland-session
+	install_package podman-docker
+	if [[ ! -e /usr/share/debootstrap/scripts/ ]]; then
+		sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+		install_package debootstrap.noarch
+	fi
+
+	if [[ ! -e /usr/share/debootstrap/scripts/focal ]]; then
+		cd /usr/share/debootstrap/scripts/
+		sudo ln -s eoan focal
+		cd -
+	fi
+
+	install_package debian-archive-keyring 
+else
+	install_package docker 
+	install_package docker.io 
+	install_package docker-compose
+	install_package debian-archive-keyring 
+	install_package debootstrap
+	install_package gnome-session-wayland
+fi
