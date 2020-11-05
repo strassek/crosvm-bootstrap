ea321b6fa14de231b3afe7a2728e5fdd578dbaf6
Kalyan Kondapally <kalyan.kondapally@intel.com>
2020-11-04 18:19:29 -0800

    Fix container launch
    Signed-off-by: Kalyan Kondapally <kalyan.kondapally@intel.com>

--- default-config/guest/etc/profile.d/setup.sh
+++ default-config/guest/etc/profile.d/setup.sh
@@ -29,6 +29,7 @@ sudo chmod +x *.sh
 cd -
 
 echo "Setting up environment........."
+sudo chown -R $LOCAL_USER:$LOCAL_USER /intel
 if [[ ! -e /intel/bin/update-containers ]]; then
 	ln -s /intel/bin/setup-containers.sh /intel/bin/update-containers
 fi
@@ -38,22 +39,20 @@ if [[ ! -e /intel/bin/launch-container ]]; then
 fi
 
 if [[ ! -e /intel/bin/container/launch ]]; then
-	sudo ln -s /intel/bin/container/app-launcher.sh /intel/bin/container/launch
+	ln -s /intel/bin/container/app-launcher.sh /intel/bin/container/launch
 fi
 
 if [[ ! -e /intel/bin/container/launch-x ]]; then
-	sudo ln -s /intel/bin/container/app-launcher-x.sh /intel/bin/container/launch-x
+	ln -s /intel/bin/container/app-launcher-x.sh /intel/bin/container/launch-x
 fi
 
 if [[ ! -e /intel/bin/container/launch-h ]]; then
-	sudo ln -s /intel/bin/container/headless.sh /intel/bin/container/launch-h
+	ln -s /intel/bin/container/headless.sh /intel/bin/container/launch-h
 fi
 
 if [[ ! -e /intel/bin/container/igt_run ]]; then
-	sudo ln -s /intel/bin/container/igt_run.sh /intel/bin/container/igt_run
+	ln -s /intel/bin/container/igt_run.sh /intel/bin/container/igt_run
 fi
-
-sudo chown -R $LOCAL_USER:$LOCAL_USER /intel
 
 cp /home/$LOCAL_USER/.env_conf/stable_release.env /home/$LOCAL_USER/.bash_env_settings
 source /home/$LOCAL_USER/.bash_aliases
